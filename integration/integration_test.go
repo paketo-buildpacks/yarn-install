a6d0cf894a810460f7ddb10d0cf7daae9030f49a
Danny Joyce <djoyce@pivotal.io>
2019-02-04 16:11:12 -0500

    Bump dagger and fix int tests [#163595064]
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -17,43 +17,46 @@ func TestIntegration(t *testing.T) {
 }
 
 func testIntegration(t *testing.T, when spec.G, it spec.S) {
+	var (
+		bp     string
+		nodeBP string
+	)
+
 	it.Before(func() {
 		RegisterTestingT(t)
+
+		var err error
+
+		bp, err = dagger.PackageBuildpack()
+		Expect(err).ToNot(HaveOccurred())
+
+		nodeBP, err = dagger.GetLatestBuildpack("nodejs-cnb")
+		Expect(err).ToNot(HaveOccurred())
 	})
 
 	when("when the node_modules are vendored", func() {
 		it("should build a working OCI image for a simple app", func() {
-			bp, err := dagger.PackageBuildpack()
-			Expect(err).ToNot(HaveOccurred())
-
-			nodeBP, err := dagger.GetRemoteBuildpack("https://github.com/cloudfoundry/nodejs-cnb/releases/download/v0.0.2/nodejs-cnb.tgz")
-			Expect(err).ToNot(HaveOccurred())
-
 			app, err := dagger.PackBuild(filepath.Join("fixtures", "simple_app_vendored"), nodeBP, bp)
 			Expect(err).ToNot(HaveOccurred())
 			defer app.Destroy()
 
 			Expect(app.Start()).To(Succeed())
 
-			Expect(app.HTTPGet("/")).To(Succeed())
+			_, _, err = app.HTTPGet("/")
+			Expect(err).ToNot(HaveOccurred())
 		})
 	})
 
 	when("when the node_modules are not vendored", func() {
 		it("should build a working OCI image for a simple app", func() {
-			bp, err := dagger.PackageBuildpack()
-			Expect(err).ToNot(HaveOccurred())
-
-			nodeBP, err := dagger.GetRemoteBuildpack("https://github.com/cloudfoundry/nodejs-cnb/releases/download/v0.0.2/nodejs-cnb.tgz")
-			Expect(err).ToNot(HaveOccurred())
-
 			app, err := dagger.PackBuild(filepath.Join("fixtures", "simple_app"), nodeBP, bp)
 			Expect(err).ToNot(HaveOccurred())
 			defer app.Destroy()
 
 			Expect(app.Start()).To(Succeed())
 
-			Expect(app.HTTPGet("/")).To(Succeed())
+			_, _, err = app.HTTPGet("/")
+			Expect(err).ToNot(HaveOccurred())
 		})
 	})
 }
