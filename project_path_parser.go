c52838ed00c161dd9febde7508cf6c6d095a1f5d
Frankie Gallina-Jones <frankieg@vmware.com>
2021-03-04 15:38:42 +0000

    Add godoc for ProjectPathParser
    Signed-off-by: Arjun Sreedharan <asreedharan@vmware.com>
    Co-authored-by: Arjun Sreedharan <asreedharan@vmware.com>

--- project_path_parser.go
+++ project_path_parser.go
@@ -7,12 +7,18 @@ import (
 	"path/filepath"
 )
 
+// ProjectPathParser provides a mechanism for determining the proper working
+// directory for the build process.
 type ProjectPathParser struct{}
 
+// NewProjectPathParser creates an instance of a ProjectPathParser.
 func NewProjectPathParser() ProjectPathParser {
 	return ProjectPathParser{}
 }
 
+// Get will resolve the absolute path of the directory specified by the
+// $BP_NODE_PROJECT_PATH environment variable. It assumes that
+// $BP_NODE_PROJECT_PATH is relative to the provided path argument.
 func (p ProjectPathParser) Get(path string) (string, error) {
 	customProjPath := os.Getenv("BP_NODE_PROJECT_PATH")
 	if customProjPath == "" {
@@ -25,9 +31,8 @@ func (p ProjectPathParser) Get(path string) (string, error) {
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return "", fmt.Errorf("expected value derived from BP_NODE_PROJECT_PATH [%s] to be an existing directory", path)
-		} else {
-			return "", err
 		}
+		return "", err
 	}
 	return path, nil
 }
